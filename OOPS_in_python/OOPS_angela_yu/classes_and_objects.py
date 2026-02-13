5b01914aa05d9cbc6118dddfc8d8976c04e92441
tech-ml-quant-luv <luvratan.techengineer@gmail.com>
2026-02-14 01:34:11 +0530

    microservices project starts

---
+++ OOPS_in_python/OOPS_angela_yu/classes_and_objects.py
@@ -0,0 +1,5 @@
+# An object has two properties
+#   - What it has   - These are known as attributes of an object
+#   - What is does  - These are known as the methods of an object. These are basically functions, but we call it methods because they are linked to an object/class
+
+# So essentially, an object is just a way of combining some piece of data and some functionality
---
+++ OOPS_in_python/OOPS_angela_yu/main.py
@@ -0,0 +1,13 @@
+from turtle import  Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+my_screen = Screen()
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+print(my_screen)
+my_screen.exitonclick()
+
